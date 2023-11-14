cb3fccf006d0ad9c56be036f2bafec1f440c228e
rayyanekaputra <rayyan.ekaputra@gmail.com>
2023-11-15 01:34:37 +0800

    fix: added more WebDriverException errors to catch, not exceptions

--- ProxyTest.py
+++ ProxyTest.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-import undetected_chromedriver as uc 
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+import undetected_chromedriver as uc
 import time
 
 import sys
@@ -15,22 +15,40 @@ def ProxyTester():
         options = uc.ChromeOptions() 
         options.add_argument(f'--proxy-server={PROXY}')
         options.add_argument("--ignore-certificate-errors")
+        # options.add_argument("--headless")
         print(f'--proxy-server={PROXY}')
         driver = webdriver.Chrome(options=options)
-        driver.implicitly_wait(1) #Menunggu DOM load
+        driver.implicitly_wait(5) #Menunggu DOM load
         driver.get("https://whatismyipaddress.com/")
        
         
         try:
             ISP = driver.find_element(by = By.XPATH, value= '//*[@id="fl-post-111"]/div/div/div[1]/div/div[2]/div/div/div/div/div/div[2]/div[1]/div[3]/div/p[1]/span[2]') 
-        except NoSuchElementException:
-            print("IP Checker Website is Unreachable")
-            
-        print("Checking jika ISP berubah..")
-        if ISP.text.strip() != "Universitas Hasanuddin":
-            print("\nISP Berubah[v]\nProxy Berhasil!")
+            print("Checking jika ISP berubah..")
+            if ISP.text.strip() != "Universitas Hasanuddin":
+                print("\nISP Berubah[v]\nProxy Berhasil!")
+                driver.quit()
+                return PROXY
+            else:
+                print("\nISP Tetap[x]\nProxy Gagal!")
             driver.quit()
-            return PROXY
-        else:
-            print("\nISP Tetap[x]\nProxy Gagal!")
-            driver.quit()
+        except WebDriverException as e:
+            if "ERR_CONNECTION_RESET" in str(e): # 'in' krn di dalamnya bukan satu string
+                print("Connection Reset, mencari proxy baru\n")
+                continue
+            elif "ERR_CONNECTION_TIMED_OUT" in str(e): 
+                print("Connection Timed Out, mencari proxy baru\n")
+                continue
+            elif "ERR_CONNECTION_CLOSED" in str(e): 
+                print("Connection Closed, mencari proxy baru\n")
+                continue
+            elif "ERR_PROXY_CONNECTION_FAILED" in str(e): 
+                print("Proxy Connection Failed, mencari proxy baru\n")
+                continue
+            else:
+                print("ERROR UNIK")
+                print("\n" + str(e))
+                continue
+        
+        
+ProxyTester()
